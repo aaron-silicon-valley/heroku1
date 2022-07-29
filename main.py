196700ba97621e20d95459d8e43e8e8c102b9eea
aaron <aaronruban60+git@gmail.com>
2022-07-28 23:42:13 -0500

    feature 1

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 
-from flask import Flask
-
+from flask import Flask, redirect
+from time import sleep
 
 app = Flask(__name__)
+
+@app.route("/")
+def navigate():
+    sleep(1)
+    return redirect("/home")
 
 @app.route("/home")
 def home():
     return "flask running"
 
-
-
+@app.route("/resume")
+def resume():
+    return redirect("https://aaron-portfolio.notion.site/Resume-ea4d47dc2f68414f8e7434b13a8e06dd")
 
 if __name__ == "__main__":
     app.run()
